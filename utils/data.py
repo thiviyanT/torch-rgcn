b806541d9c4aef353596da620fec2091ae499731
Thiviyan Thanapalasingam <thiviyan.t@gmail.com>
2020-07-02 09:52:30 +0200

    Add graph pruning

--- utils/data.py
+++ utils/data.py
@@ -16,23 +16,37 @@ def st(node):
     we use .n3() for everything else, so that different nodes don't become unified.
 
     Source: https://github.com/pbloem/gated-rgcn/blob/1bde7f28af8028f468349b2d760c17d5c908b58b/kgmodels/data.py#L16
-
-    :param node:
-    :return:
     """
     if type(node) == URIRef:
         return str(node)
     else:
         return node.n3()
 
+def add_neighbors(set, graph, node, depth=2):
+    """
+    Adds neighboring nodes that are n nodes away from the target label
+
+    Source: https://github.com/pbloem/gated-rgcn/blob/f5031a3cbb485aab964aea7b6856d0584155c820/kgmodels/data.py#L29
+    """
+    if depth == 0:
+        return
+
+    for s, p, o in graph.triples((node, None, None)):
+        set.add((s, p, o))
+        add_neighbors(set, graph, o, depth=depth-1)
+
+    for s, p, o in graph.triples((None, None, node)):
+        set.add((s, p, o))
+        add_neighbors(set, graph, s, depth=depth-1)
+
 def load_strings(file):
-    """ Read triples file """
+    """ Read triples from file """
     with open(file, 'r') as f:
         return [line.split() for line in f]
 
 
 # TODO: May need to rewrite this without RDFlib - Low priority
-def load_node_classification_data(name, use_test_set=False, limit=None, disable_cache=True, val_prop=0.4, prune=False):
+def load_node_classification_data(name, use_test_set=False, limit=None, enable_cache=True, val_prop=0.4, prune=False):
     """
     Load knowledge graphs for node classification experiment.
 
@@ -42,17 +56,20 @@ def load_node_classification_data(name, use_test_set=False, limit=None, disable_
     :param use_test_set: If true, load the canonical test set, otherwise split a validation set off from the training data.
     :param limit: If set, the number of unique relations will be limited to this value, plus one for the self-connections,
                   plus one for the remaining connections combined into a single, new relation.
+    :param prune: Whether to prune edges that are further than two steps from the target labels
     :param disable_cache: If true, does not load from cache.
     :param val_prop: Size of the validation split from the training data (Default: 0.4).
-    :return: A tuple containing the graph data, and the node classification test and train sets:
-              - edges: dictionary of edges (relation -> pair of lists cont. subject and object indices respectively)
+    :return: A list containing the graph data, and the node classification test and train sets:
+              - edges: list of edges [subject, predicate object]
     """
     REST = '.rest'
     INV = 'inv.'
 
     # Check if the data has been cached for quick loading.
-    cachefile = locate_file(f'data{S}{name}{S}cache_{"test" if use_test_set else "validation"}.pkl')
-    if not disable_cache and os.path.isfile(cachefile) and limit is None:
+    cachefile = locate_file(f'data{S}{name.lower()}{S}cache'
+                            f'{"_test" if use_test_set else "_validation"}'
+                            f'{"_prune" if prune else ""}.pkl')
+    if enable_cache and os.path.isfile(cachefile) and limit is None:
         print('Using cached data...')
         with open(cachefile, 'rb') as file:
             data = pickle.load(file)
@@ -122,7 +139,13 @@ def load_node_classification_data(name, use_test_set=False, limit=None, disable_
     print('RDF loaded.')
 
     # Collect all node and relation labels
-    triples = graph
+    if prune:
+        triples = set()
+        for node in list(train.keys()) + list(test.keys()):
+            add_neighbors(triples, graph, URIRef(node), depth=2)
+
+    else:
+        triples = graph
 
     nodes = set()
     relations = Counter()
@@ -155,7 +178,7 @@ def load_node_classification_data(name, use_test_set=False, limit=None, disable_
     print('Graph loaded.')
 
     # Cache the results for fast loading next time
-    if limit is None and not disable_cache:
+    if limit is None and enable_cache:
         with open(cachefile, 'wb') as file:
             pickle.dump([edges, (n2i, i2n), (r2i, i2r), train, test], file)
 
@@ -170,8 +193,10 @@ def load_link_prediction_data(name, use_test_set=False, limit=None):
     :param name: Dataset name ('aifb', 'am', 'bgs' or 'mutag')
     :param use_test_set: If true, load the canonical test set, otherwise load validation set from file.
     :param limit: If set, only the first n triples are used.
-    :return: A tuple containing the graph data, and the relation Prediction  test and train sets:
-              - edges: dictionary of edges (relation -> pair of lists cont. subject and object indices respectively)
+    :return: Relation prediction test and train sets:
+              - train: list of edges [subject, predicate object]
+              - test: list of edges [subject, predicate object]
+              - all_triples: sets of tuples (subject, predicate object)
     """
 
     if name.lower() == 'fb15k':
