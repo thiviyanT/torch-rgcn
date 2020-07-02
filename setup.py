4267578eb6e68f9faff3fbaf8248c8bae04a9e4e
Thiviyan Thanapalasingam <thiviyan.t@gmail.com>
2020-07-02 09:49:22 +0200

    Rename rgcn to torch-rgcn

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='torch-rgcn',
-      version='0.1',
+      version='1.0',
       description='A PyTorch library for Relational Graph Convolutional Networks',
       url='https://github.com/thiviyanT/torch-RGCN',
       author='Thiviyan Thanapalasingam',
       author_email='t.singam@uva.nl',
       license='MIT',
-      packages=['rgcn'],
+      packages=['torch_rgcn'],
       python_requires='>=3.7',
       zip_safe=False)
